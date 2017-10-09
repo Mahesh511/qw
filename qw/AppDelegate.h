21ed1176d66eb9334cb24437cdb9fc23220632dd
BiipByte <biipbyte@BiipBytes-Mac-mini.local>
2017-10-09 11:22:32 +0530

    Initial Commit

---
+++ qw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qw
+//
+//  Created by BiipByte on 10/9/17.
+//  Copyright © 2017 BiipByte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qw
+//
+//  Created by BiipByte on 10/9/17.
+//  Copyright © 2017 BiipByte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
